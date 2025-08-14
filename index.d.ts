404b690b9734c9465a22583bb138439a619b4ee7
DIEGOHORVATTI <d.horvattid@gmail.com>
2025-08-14 14:07:31 -0300

    chore: enhance index.d.ts with explicit NitroRouter exports

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,9 @@
+// Re-export all types and functions from src
 export * from './src'
+
+// Explicitly export NitroRouter class
+export { NitroRouter } from './src/nitro-router'
+
+// Default export
+import { NitroRouter } from './src/nitro-router'
+export default NitroRouter
