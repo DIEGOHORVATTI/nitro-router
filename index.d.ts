28a5d6601ae0a50a69e53659bb145dcb635804b9
DIEGOHORVATTI <d.horvattid@gmail.com>
2025-08-14 13:58:15 -0300

    chore: update index.d.ts for improved module exports

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,1 @@
-// This file ensures TypeScript can find type declarations for the 'nitro-router' package
-declare module 'nitro-router' {
-  export interface NitroRouter {
-    // Define your NitroRouter interface properties here
-  }
-  export const NitroRouter: NitroRouter
-  export default NitroRouter
-}
+export * from './src'
