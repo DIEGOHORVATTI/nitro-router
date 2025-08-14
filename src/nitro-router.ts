8157febcd2e9bf05e9d998c66b59f1d4cd18046b
DIEGOHORVATTI <d.horvattid@gmail.com>
2025-08-14 15:33:23 -0300

    feat: enhance documentation for middleware and route registration methods

--- src/nitro-router.ts
+++ src/nitro-router.ts
@@ -23,12 +23,22 @@ export class NitroRouter<Ext extends Record<string, unknown> = {}> {
     this.middlewares = options?.middlewares ?? []
   }
 
+  /**
+   * @title use method to add middleware
+   * @description This allows chaining of middlewares
+   * @returns A new NitroRouter instance with the added middleware
+   */
   use<MExt extends Record<string, unknown>>(typed: TypedMiddleware<MExt>) {
     this.middlewares.push(createUseHandler(typed))
 
     return this as NitroRouter<Ext & MExt>
   }
 
+  /**
+   * @title group method to create a new NitroRouter with a prefix and tags
+   * @description This allows creating a new router with a specific prefix and tags
+   * @returns A new NitroRouter instance with the specified prefix and tags
+   */
   group(options: Group) {
     return new NitroRouter<Ext>({
       prefix: this.prefix + (options.prefix ?? ''),
@@ -43,21 +53,45 @@ export class NitroRouter<Ext extends Record<string, unknown> = {}> {
     return createRegister<Ext>(this.middlewares, this.router, this.prefix, this.tags)
   }
 
+  /**
+   * @title Methods to register routes
+   * @description These methods allow registering routes with different HTTP methods
+   */
   get: RouteMethod<Ext> = (path, handler, options) =>
     createMethodFactory<Ext>(this._register).get(path, handler, options)
 
+  /**
+   * @title Methods to register routes
+   * @description These methods allow registering routes with different HTTP methods
+   */
   post: RouteMethod<Ext> = (path, handler, options) =>
     createMethodFactory<Ext>(this._register).post(path, handler, options)
 
+  /**
+   * @title Methods to register routes
+   * @description These methods allow registering routes with different HTTP methods
+   */
   put: RouteMethod<Ext> = (path, handler, options) =>
     createMethodFactory<Ext>(this._register).put(path, handler, options)
 
+  /**
+   * @title Methods to register routes
+   * @description These methods allow registering routes with different HTTP methods
+   */
   patch: RouteMethod<Ext> = (path, handler, options) =>
     createMethodFactory<Ext>(this._register).patch(path, handler, options)
 
+  /**
+   * @title Methods to register routes
+   * @description These methods allow registering routes with different HTTP methods
+   */
   delete: RouteMethod<Ext> = (path, handler, options) =>
     createMethodFactory<Ext>(this._register).delete(path, handler, options)
 
+  /**
+   * @title Methods to register routes
+   * @description These methods allow registering routes with different HTTP methods
+   */
   export() {
     return this.router
   }
